b5df33f047784a6eace65bd254b645b15920fff2
TSM Techø <trottimusbg@gmail.com>
2019-08-27 10:20:54 +0300

    [PlayerMovement] Add FixedUpdate method to move the player.

---
+++ Player Movement/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+
+    public Rigidbody2D RigidBody2;
+
+    private Vector2 _movement;
+
+    void Update()
+    {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        RigidBody2.MovePosition(RigidBody2.position + _movement * MoveSpeed * Time.fixedDeltaTime);
+    }
+}
